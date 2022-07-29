e55d88e6cb92efb4fee483242eaa05b9a332a49b
MladenGukic <mladengukic@gmail.com>
2022-07-29 17:23:49 +0200

    add loops

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt("Maximum life for you and the monster", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
@@ -220,6 +221,34 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
+  for (let i = 0; i < 3; i++) {
+    console.log("--------");
+  }
+  // let j = 0;
+  // while (j < 3) {
+  //   console.log("---------");
+  //   j++;
+  // }
+  // for (let i = 10; i < 3; i++) {
+  //   i--;
+  //   console.log(i);
+  // }
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i]);
+  // }
+  let i = 0;
+  for (const logEntry of battleLog) {
+    if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry === i) {
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLoggedEntry = i;
+      break;
+    }
+    i++;
+  }
+
   console.log(battleLog);
 }
 
